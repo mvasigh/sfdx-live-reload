87dc6b7be27189aeb23a57b81592c53b4c879915
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-09-15 08:36:26 -0500

    watch directory for changes and push on change

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const minimist = require('minimist');
-const fs = require('fs');
+const watch = require('watch');
+const exec = require('child_process').exec;
+const argv = require('yargs')
+  .demandCommand(0, 1, null, 'Too many commands')
+  .option('u', {
+    alias: 'org',
+    demandOption: false,
+    describe:
+      'Alias of the scratch org that you want to push to (leave blank to use default).',
+    type: 'string'
+  }).argv;
 
-module.exports = () => {
-    const args = minimist(process.argv.slice(2));
-    const path = args._[0];
-
-    return fs.existsSync(path)
-        ? require('./src/live_reload')(args)
-        : console.error(`${path} does not exist. Please enter a valid path.`)
-}
+watch.watchTree(process.cwd(), {}, (f, curr, prev) => {
+  console.clear();
+  exec(
+    `sfdx force:source:push ${argv.u ? '-u ' + argv.u : ''} --json`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+      }
+      const result = JSON.parse(stdout);
+      // use result to refresh browser window containing org url
+      console.log(result);
+    }
+  );
+});
